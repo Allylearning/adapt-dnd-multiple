3484f51798fe3cec3075abe46585afe5d64a9ec5
Ally Lundie <ally.lundie.co.uk>
2025-07-01 15:08:27 +0100

    Nested controls update

--- templates/dndMultipleDraggable.jsx
+++ templates/dndMultipleDraggable.jsx
@@ -34,58 +34,85 @@ export default function DndMultipleDragable({
 
   return (
     <Draggable draggableId={title} index={index} isDragDisabled={!isEnabled}>
-      {(provided) => (
-        <div
-          className={classes([
-            'dnd-multiple__dragitem',
-            _graphic.isBackground && 'has-graphic-background',
-            isCorrect && 'is-correct',
-            !isCorrect && isCorrect !== undefined && 'is-incorrect',
-            isSelected && 'is-selected'
-          ])}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          aria-hidden={onSelect ? undefined : true}
-          tabIndex={onSelect ? undefined : -1}
-        >
-          {DragItemGraphic()}
-          <span
-            className='dnd-multiple__dragitem__title'
-            dangerouslySetInnerHTML={{ __html: title }}
-          ></span>
-          <div className='dnd-multiple__dragitem__state'>
-            <div
-              className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__correct-icon'
-              aria-label={_globals._accessibility._ariaLabels.correct}
-            >
-              <div className='icon' aria-hidden='true'></div>
+      {(provided) =>
+        isEnabled && onSelect ? (
+          <button
+            type='button'
+            className={classes([
+              'dnd-multiple__dragitem',
+              _graphic.isBackground && 'has-graphic-background',
+              isCorrect && 'is-correct',
+              !isCorrect && isCorrect !== undefined && 'is-incorrect',
+              isSelected && 'is-selected'
+            ])}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            onClick={(e) => {
+              e.stopPropagation();
+              onSelect();
+            }}
+            aria-label={isSelected ? `${title} selected` : `Select ${title}`}
+          >
+            {DragItemGraphic()}
+            <span
+              className='dnd-multiple__dragitem__title'
+              dangerouslySetInnerHTML={{ __html: title }}
+            ></span>
+            <div className='dnd-multiple__dragitem__state'>
+              <div
+                className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__correct-icon'
+                aria-label={_globals._accessibility._ariaLabels.correct}
+              >
+                <div className='icon' aria-hidden='true'></div>
+              </div>
+              <div
+                className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__incorrect-icon'
+                aria-label={_globals._accessibility._ariaLabels.incorrect}
+              >
+                <div className='icon' aria-hidden='true'></div>
+              </div>
             </div>
-            <div
-              className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__incorrect-icon'
-              aria-label={_globals._accessibility._ariaLabels.incorrect}
-            >
-              <div className='icon' aria-hidden='true'></div>
+            <span className='drag-icon' aria-hidden='true'></span>
+          </button>
+        ) : (
+          <div
+            className={classes([
+              'dnd-multiple__dragitem',
+              _graphic.isBackground && 'has-graphic-background',
+              isCorrect && 'is-correct',
+              !isCorrect && isCorrect !== undefined && 'is-incorrect',
+              isSelected && 'is-selected'
+            ])}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            aria-hidden='true'
+            tabIndex={-1}
+          >
+            {DragItemGraphic()}
+            <span
+              className='dnd-multiple__dragitem__title'
+              dangerouslySetInnerHTML={{ __html: title }}
+            ></span>
+            <div className='dnd-multiple__dragitem__state'>
+              <div
+                className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__correct-icon'
+                aria-label={_globals._accessibility._ariaLabels.correct}
+              >
+                <div className='icon' aria-hidden='true'></div>
+              </div>
+              <div
+                className='dnd-multiple__dragitem__icon dnd-multiple__dragitem__incorrect-icon'
+                aria-label={_globals._accessibility._ariaLabels.incorrect}
+              >
+                <div className='icon' aria-hidden='true'></div>
+              </div>
             </div>
+            <span className='drag-icon' aria-hidden='true'></span>
           </div>
-          {isEnabled && onSelect && (
-            <button
-              type='button'
-              className={classes([
-                'dnd-multiple__select-icon',
-                isSelected && 'is-selected'
-              ])}
-              onClick={(e) => {
-                e.stopPropagation();
-                onSelect();
-              }}
-              aria-label={isSelected ? `${title} selected` : `Select ${title}`}
-            >
-              <span className='drag-icon' aria-hidden='true'></span>
-            </button>
-          )}
-        </div>
-      )}
+        )
+      }
     </Draggable>
   );
 }
